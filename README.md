8ac389f73a4af786552a81165491577eb1065775
Mahmoud Abdelkawy <78190964+m7moud2@users.noreply.github.com>
2023-02-14 20:38:37 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# loge-in
-loge in
+# oms
+
+A Flutter project on Login , Registration and rest password screens.
+<br>
+<br>
+<img src='screenshots/Login.jpg' width="200">
+<br>
+
+<br>
+<img src='screenshots/register.jpg' width="200">
+<br>
+
+<br>
+<img src='screenshots/reset.jpg' width="200">
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:oms/login.dart';
+import 'package:oms/register.dart';
+import 'package:oms/resetpass.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'login',
+      title: 'mahmoud',
+      routes: {
+        'login': (context) => MyLogin(),
+        'register': (context) => myRegister(),
+        'forgot': (context) => resetPassword(),
+      },
+    ),
+  );
+}
